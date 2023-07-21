3c78928ee7a20b2d0bb690a095959879add4ccc4
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2023-07-21 17:20:55 +0100

    ui: add image

--- components/Nav/Profile.vue
+++ components/Nav/Profile.vue
@@ -1,3 +1,11 @@
 <template>
-  <span class="bg-yellow-500 w-8 h-8 rounded-full block"></span>
+  <div
+    class="bg-background-200 w-8 h-8 rounded-full block cursor-pointer hover:ring-1 ring-primary-dark dark:ring-primary-light"
+  >
+    <img
+      class="w-full h-full object-cover"
+      src="https://robohash.org/91f2ffa286f30321f2a514810c957e57?set=set4&bgset=&size=50x50"
+      alt="Profile"
+    />
+  </div>
 </template>
