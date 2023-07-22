2277f64ad6db41ce63454b96708a76580de6dd35
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2023-07-22 10:15:32 +0100

    feat: setup server

---
+++ server/api/search/history/index.get.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler(event => {
+  let history = [
+    { id: "c00476c5-92ac-5ee2-9813-18549edd0a18", text: "twitter" },
+    { id: "ad715ed3-4195-59e2-88b1-b9ae97dd90a2", text: "google" },
+    { id: "6edaab43-6090-5761-8339-e62c9506143c", text: "whatsapp" },
+    { id: "64f69455-e68e-57c1-9fe5-285d27fe009c", text: "instagram" },
+    { id: "47d313c1-5c90-5b41-8a96-2e7ed8087632", text: "facebook" },
+  ];
+  return history;
+});
