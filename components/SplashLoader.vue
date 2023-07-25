6902a1dc8e79117a5d9988f65fe9f4db7c8ec2c4
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2023-07-25 14:45:58 +0100

    feat: include dynamic component in SplashLoader.vue component

--- components/SplashLoader.vue
+++ components/SplashLoader.vue
@@ -1,21 +1,26 @@
 <template>
+  <Html data-color-mode="orange" :class="useThemeMode().value" lang="en">
   <Title> Loading... | {{ useTitle().value }}</Title>
 
   <Body>
 
-    <main class="h-screen flex items-end justify-center">
+  <div class="hidden">
+    <NavTheme />
+  </div>
+    <main class="h-screen flex items-end justify-center bg-background">
       <section class="h-[calc(50%+3rem)] space-y-48 text-center">
-      <div>
-        <Logo />
-      </div>
+        <div>
+          <Logo />
+        </div>
 
-      <div>
-        <Icon
-          name="svg-spinners:ring-resize"
-          class="text-foreground-display text-4xl"
-          />
+        <div>
+          <Icon name="svg-spinners:ring-resize" class="text-foreground-display text-4xl" />
         </div>
       </section>
     </main>
   </Body>
+
+  </Html>
 </template>
+<script setup lang="ts">
+</script>
