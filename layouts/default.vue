21674a3d21f085841fa9d10252d007e82e0a2754
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2023-07-25 14:49:02 +0100

    ui: fix layout hierarchy

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,29 @@
 <template>
   <Html data-color-mode="orange" :class="useThemeMode().value" lang="en">
-    <Head>
-      <Title>{{ useTitle().value }}</Title>
-    </Head>
 
-    <Body class="bg-background text-foreground-body">
-      <!-- <div class="p-32"></div> -->
-      <section class="w-screen max-w-[2500px] mx-auto max-h-screen flex flex-col">
-        <div>
-          <Nav />
-        </div>
+  <Head>
+    <Title>{{ useTitle().value }}</Title>
+  </Head>
 
-        <section class="overflow-hidden h-full flex">
-          <section class="w-20">
-            <Sidebar />
-          </section>
+  <Body class="bg-background text-foreground-body">
+    <!-- <div class="p-32"></div> -->
+    <section class="w-screen max-w-[2500px] mx-auto max-h-screen flex flex-col">
+      <div class="z-50">
+        <Nav />
+      </div>
 
-          <main
-            class="overflow-y-auto z-10 overflow-x-hidden min-h-screen mx-auto w-full  bg-background-200 rounded-tl-md scrollbar-thin scrollbar-thumb-primary dark:scrollbar-thumb-primary-light scrollbar-track-background scrollbar-thumb-rounded-lg"
-          >
-            <slot />
-          </main>
+      <section class="overflow-hidden h-full flex">
+        <section class="w-20 px-2">
+          <Sidebar />
         </section>
+
+        <main
+          class="z-10 overflow-y-auto overflow-x-hidden min-h-screen mx-auto w-full  bg-background-200 rounded-tl-md scrollbar-thin scrollbar-thumb-primary dark:scrollbar-thumb-primary-light scrollbar-track-background scrollbar-thumb-rounded-lg">
+          <slot />
+        </main>
       </section>
-    </Body>
+    </section>
+  </Body>
+
   </Html>
 </template>
