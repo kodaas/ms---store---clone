ae25898a34c13a227f1ce15b7e977655bcae50f5
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2023-07-25 14:48:09 +0100

    fix: build bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  ssr: false,
+
   css: ["~/assets/css/main.css"],
 
   modules: ["nuxt-icon", "@vueuse/nuxt"],
+
+  spaLoadingTemplate: false,
 
   postcss: {
     plugins: {
